97c21f35231ef62808ac80f9a3d68283a7badb72
hwicoding <dlgnl117@gmail.com>
2025-09-16 20:08:47 +0900

    250916 > front-end > flutter> test: home.dart 앱바 제목 변경 재 테스트

--- front-end/flutter/doctor_goblin/lib/home.dart
+++ front-end/flutter/doctor_goblin/lib/home.dart
@@ -7,7 +7,7 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return  Scaffold(
       appBar: AppBar(
-        title: Text('제발 통과!!!!'),
+        title: Text('몇번째냐!'),
       ),
     );
   }
