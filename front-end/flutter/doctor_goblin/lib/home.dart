9cf67e4a7e524d2a10fa16000b0fc1ef7650ec52
hwicoding <dlgnl117@gmail.com>
2025-09-16 20:35:16 +0900

    250916 > front-end > flutter> test: home.dart 앱바 제목 변경 재 테스트 및 포트 변경

--- front-end/flutter/doctor_goblin/lib/home.dart
+++ front-end/flutter/doctor_goblin/lib/home.dart
@@ -7,7 +7,7 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return  Scaffold(
       appBar: AppBar(
-        title: Text('하....진심 몇번째냐!'),
+        title: Text('될 가능성이 높다!'),
       ),
     );
   }
